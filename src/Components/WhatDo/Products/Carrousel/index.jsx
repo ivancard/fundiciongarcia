648d4560062d897dfa4accf1759beadedafaf47f
ivancard <ivaanlml@gmail.com>
2022-05-25 16:24:51 -0300

    buttons of scroll products implemented

--- src/Components/WhatDo/Products/Carrousel/index.jsx
+++ src/Components/WhatDo/Products/Carrousel/index.jsx
@@ -1,40 +1,26 @@
+import { useRef } from 'react';
 import { CarrouselItem } from './CarrouselItem';
 import image1 from '../../../../assets/img/product-1.jpg';
 
 import './carrousel.scss';
 
 export const Carrousel = () => {
+    const carrousel = useRef(null);
+
+    const handleLeftClick = () => {
+        // carrousel.current.scrollLeft -= 285;
+        carrousel.current.scrollLeft -= carrousel.current.offsetWidth;
+    };
+    const handleRightClick = () => {
+        carrousel.current.scrollLeft += carrousel.current.offsetWidth;
+        // carrousel.current.scrollLeft += 285;
+    };
     return (
         <div className='carrousel'>
-            <div className='product-icon-row row-right'>
-                <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='40'
-                    height='40'
-                    fill='currentColor'
-                    className='bi bi-arrow-left-circle-fill product-icon-row row-right'
-                    viewBox='0 0 16 16'>
-                    <path d='M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z' />
-                </svg>
-            </div>
-
-            <div className='carrousel-container'>
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-                <CarrouselItem img={image1} description='hoa' />
-            </div>
-            <div
+            <button
+                onClick={handleLeftClick}
                 className='product-icon-row row-right'
-                style={{ transform: 'rotate(180deg)' }}>
+                style={{ border: 'none' }}>
                 <svg
                     xmlns='http://www.w3.org/2000/svg'
                     width='40'
@@ -44,7 +30,36 @@ export const Carrousel = () => {
                     viewBox='0 0 16 16'>
                     <path d='M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z' />
                 </svg>
+            </button>
+
+            <div className='carrousel-container' ref={carrousel}>
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
+                <CarrouselItem img={image1} description='hoa' />
             </div>
+            <button
+                onClick={handleRightClick}
+                className='product-icon-row row-right'
+                style={{ transform: 'rotate(180deg)', border: 'none' }}>
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='40'
+                    height='40'
+                    fill='currentColor'
+                    className='bi bi-arrow-left-circle-fill product-icon-row row-right'
+                    viewBox='0 0 16 16'>
+                    <path d='M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z' />
+                </svg>
+            </button>
         </div>
     );
 };
