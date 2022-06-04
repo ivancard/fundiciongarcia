ddaea88ebef1cd9a734e0691e031959ea352beec
ivancard <ivaanlml@gmail.com>
2022-06-03 22:31:04 -0300

    slider developed

--- src/Components/Feet/index.jsx
+++ src/Components/Feet/index.jsx
@@ -1,14 +1,77 @@
-import pata1 from '../../assets/img/pata-1-low.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Scrollbar } from 'swiper';
+import 'swiper/css';
+
 import './feet.scss';
+
+import patas from '../../data/dataPatas';
+import { useState, useRef } from 'react';
+
 export const Feet = () => {
+    // const [pataArr, setpataArr] = useState(patas);
+    // const pataHtml = useRef(null);
+
+    // const handleRight = (e) => {
+    //     e.preventDefault();
+    //     pataHtml.current.scrollLeft += pataHtml.current.offsetWidth / 5;
+    //     // const patas = pataHtml.current.childNodes;
+    //     // patas.forEach((element) => {
+    //     //     if (!element.classList.contains('pata-right')) {
+    //     //         element.classList.add('pata-right');
+    //     //     } else {
+    //     //         element.classList.remove('pata-right');
+    //     //     }
+    //     // });
+    //     const auxPataArr = [...pataArr];
+    //     auxPataArr.push(auxPataArr.shift());
+    //     setpataArr(auxPataArr);
+    // };
+
+    // const handleLeft = (e) => {
+    //     pataHtml.current.scrollLeft -= 50;
+    //     // const auxPataArr = [...pataArr];
+    //     // auxPataArr.unshift(auxPataArr.pop());
+    //     // setpataArr(auxPataArr);
+    // };
+
     return (
         <div className='feet-container'>
             <h3>Patas de sillones</h3>
             <div className='card-patas-container'>
-                <div className='card-pata'>
+                <Swiper
+                    modules={[Scrollbar]}
+                    spaceBetween={20}
+                    slidesPerView={3}
+                    loop={true}
+                    // onSlideChange={() => console.log('slide change')}
+                    // onSwiper={(swiper) => console.log(swiper)}
+                >
+                    {/* <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide> */}
+                    {patas.map(({ id, title, img }) => (
+                        <SwiperSlide key={id}>
+                            <div className='card-pata'>
+                                <img
+                                    className='img-pata'
+                                    src={img}
+                                    alt={title}
+                                />
+                                <span className='pata'>{title}</span>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+
+                {/* <div className='card-pata'>
                     <img src={pata1} alt='pata1' />
-                    <h5>pata1</h5>
-                </div>
+                    <h5>psata1</h5>
+                </div> */}
             </div>
         </div>
     );
---
+++ src/data/dataPatas.js
@@ -0,0 +1,17 @@
+import imgPata1 from '../assets/img/patas/pata-1.jpg';
+import imgPata2 from '../assets/img/patas/pata-2.jpg';
+import imgPata3 from '../assets/img/patas/pata-3.jpg';
+import imgPata4 from '../assets/img/patas/pata-4.jpg';
+import imgPata5 from '../assets/img/patas/pata-5.jpg';
+import imgPata6 from '../assets/img/patas/pata-6.jpg';
+import imgPata7 from '../assets/img/patas/pata-7.jpg';
+
+export default [
+    { id: 1, title: 'pata bocha', img: imgPata1 },
+    { id: 2, title: 'pata Berlin', img: imgPata2 },
+    { id: 3, title: 'pata L', img: imgPata3 },
+    { id: 4, title: 'pata Stylo', img: imgPata4 },
+    { id: 5, title: 'pata taco corto', img: imgPata5 },
+    { id: 6, title: 'pata vicera larga', img: imgPata6 },
+    { id: 7, title: 'pata tipo Y ', img: imgPata7 },
+];
